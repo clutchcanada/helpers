1075a0ac52ef076be739b85a13b3171f02a667da
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-06-11 14:01:19 -0400

    whiteSpaceValidation testing

--- src/validation/index.js
+++ src/validation/index.js
@@ -4,5 +4,5 @@ export { default as postCodeValidation } from './post-code';
 export { default as futureDateValidation } from './futureDate';
 export { default as pastDateValidation } from './pastDate';
 export { default as nameValidation } from './name';
-export { default as whiteSpaceValidation } from './white-space';
+export { default as whiteSpaceValidation } from './whiteSpace';
 export { default as emailValidation } from './email';
