89b75c5fbb491d6f5e9aa2d27635e1b098361c8d
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-06-11 14:52:43 -0400

    updated export

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,6 +1,6 @@
 export { default as hasValue } from './hasValue';
 export { default as dateValidation } from './date';
-export { default as postCodeValidation } from './post-code';
+export { default as postCodeValidation } from './postCode';
 export { default as futureDateValidation } from './futureDate';
 export { default as pastDateValidation } from './pastDate';
 export { default as nameValidation } from './name';
