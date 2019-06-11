a85b6fa8847190e9f4e8c8721b42524622f81fdb
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-06-11 14:04:47 -0400

    postCodeValidation testing

---
+++ src/validation/postCode/index.test.js
@@ -0,0 +1,16 @@
+import postCodeValidation from "./index";
+
+describe("postCodeValidation", () => {
+  it('should return true if valid postCode', () => {
+    expect(postCodeValidation("M3H8F3")).toBe(true);
+  });
+  
+  it('should return false if inValid postCode', () => {
+    expect(postCodeValidation("M338FT")).toBe(false);
+  });
+
+  it('should work regardless of spaces', () => {
+    expect(postCodeValidation("M3H8F3")).toBe(true);
+    expect(postCodeValidation("M3H 8F3")).toBe(true);
+  });
+});
