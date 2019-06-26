835ff43d9af877a8b7a4a9d9718fcdcee4ea20d2
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-06-26 14:08:07 -0400

    removeNonNumbers

---
+++ src/removeNonNumbers/index.test.js
@@ -0,0 +1,10 @@
+import removeNonNumbers from "./index";
+
+
+describe("removeNonNumbers", () => {
+  it('should remove non numbers from strings', () => {
+    expect(removeNonNumbers("234sdfhs_dsfh7")).toBe("2347");
+    expect(removeNonNumbers("2qwe43gfdgdfsfh334")).toBe("243334");
+    expect(removeNonNumbers("5__^^^_dsfh%")).toBe("5");
+  });
+});
