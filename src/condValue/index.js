9e07ed03f6444551ec1eb7fc081776c5d0385233
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-07-02 17:02:55 -0400

    remove condValue

--- src/condValue/index.js
+++
@@ -1,8 +0,0 @@
-import * as R from "ramda";
-
-export default (conditionsArray) => {
-  const conditionsArrayToPassToRamda = conditionsArray.map(
-      ([condition, valueToReturn]) => [condition, R.always(valueToReturn)]
-  );
-  return R.cond(conditionsArrayToPassToRamda);
-}
