3707cf046115d7a15fc8c0cbb92b68bfbf86a08d
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-07-03 12:27:03 -0400

    refactor

--- src/snakeCaseToReadableString/index.js
+++ src/snakeCaseToReadableString/index.js
@@ -1,6 +1,10 @@
-const snakeCaseToReadableString = (objectKey) => {
-    const parsedString = objectKey.replace(/_/g, " ").toLowerCase();
-    return parsedString.charAt(0).toUpperCase() + parsedString.slice(1); 
+const capitalizeString = value => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+
+const snakeCaseToReadableString = (key = '') =>  {
+    const readableString = key.split('_')
+    .map(value => value.toLowerCase())
+    .join(' ');
+    return capitalizeString(readableString);
 }
 
 export default snakeCaseToReadableString;
