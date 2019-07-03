e0482d7ce4f49abe0eca3b4147cbbe8dd5074467
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-07-03 12:02:43 -0400

    use more performant version of number mask

--- src/masks/number/index.test.js
+++ src/masks/number/index.test.js
@@ -2,8 +2,8 @@ import numberMask from "./index";
 
 describe("numberMasking", () => {
   it('should mask numbers with commas', () => {
-    
-    expect(numberMask("1234")).toBe("1,234");
+    expect(numberMask("1234")).toBe("1,234");    
+    expect(numberMask("12345")).toBe("12,345");
     expect(numberMask("123456789")).toBe("123,456,789");
     expect(numberMask("99999999")).toBe("99,999,999");
   });
