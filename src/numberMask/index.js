58bf706f9c4ef8e7a4c8152b6cc1fcc2332a2f86
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-09-06 16:40:25 -0400

    add toString on arguement and bump version

--- src/numberMask/index.js
+++ src/numberMask/index.js
@@ -1,6 +1,7 @@
 import * as R from "ramda";
 import removeNonNumbers from "../removeNonNumbers";
 
-const addCommasToNumber = (value = '') => value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+const addCommasToNumber = (value = '') => value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
 
 export default R.pipe(removeNonNumbers, addCommasToNumber);
