82583c9a296e7cc719a860eee666b3a766f07c21
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-09-19 14:23:53 -0400

    sort alphabetically

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-export { default as throwError } from './throwError';
 export { default as addKey } from './addKey';
-export { default as extractValueFromEvent } from './extractValueFromEvent';
-export { default as extractKey } from './extractKey';
-export {
-  default as createValidationFunction,
-} from './createValidationFunction';
-export { default as preventDefault } from './preventDefault';
-export { default as calculateElementAttribute } from "./calculateElementAttribute";
-export { default as persistEvent } from "./persistEvent";
-export { default as reconstructEvent } from "./reconstructEvent";
-export { default as isTruthy } from './isTruthy';
-export { default as zeroIfEmptyString } from "./zeroIfEmptyString";
-export { default as snakeCaseToReadableString } from "./snakeCaseToReadableString";
-export { default as removeNonNumbers } from "./removeNonNumbers";
-export { default as includes } from "./includes"
-export { default as callAll } from "./callAll";
-export { default as truthyOrZero } from "./truthyOrZero";
-export { default as emptyStringWhenLessThanZero } from "./emptyStringWhenLessThanZero";
-export { default as numberMask } from "./numberMask";
-export { default as formatPhoneNumber } from "./formatPhoneNumber";
-export * from "./validation";
-export { default as formatPrice } from "./formatPrice";
-export { default as formatMileage } from "./formatMileage";
 export { default as asyncQueue } from "./async-queue";
+export { default as calculateElementAttribute } from "./calculateElementAttribute";
+export { default as callAll } from "./callAll";
+export { default as createValidationFunction } from './createValidationFunction';
+export { default as emptyStringWhenLessThanZero } from "./emptyStringWhenLessThanZero";
+export { default as extractKey } from './extractKey';
+export { default as extractValueFromEvent } from './extractValueFromEvent';
+export { default as formatMileage } from "./formatMileage";
+export { default as formatPhoneNumber } from "./formatPhoneNumber";
+export { default as formatPrice } from "./formatPrice";
+export { default as includes } from "./includes"
+export { default as isTruthy } from './isTruthy';
 export { default as isValidNumber } from './isValidNumber';
+export { default as numberMask } from "./numberMask";
+export { default as persistEvent } from "./persistEvent";
+export { default as preventDefault } from './preventDefault';
+export { default as reconstructEvent } from "./reconstructEvent";
+export { default as removeNonNumbers } from "./removeNonNumbers";
+export { default as snakeCaseToReadableString } from "./snakeCaseToReadableString";
+export { default as throwError } from './throwError';
+export { default as truthyOrZero } from "./truthyOrZero";
+export { default as zeroIfEmptyString } from "./zeroIfEmptyString";
+export * from "./validation";
