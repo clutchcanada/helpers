9dc2149daceb30857de1bf98feb0f1fe4e9fefe2
Scott Dougan <scottdougan@me.com>
2019-09-19 11:32:31 -0400

    Update test and version number

--- src/isValidNumber/index.test.js
+++ src/isValidNumber/index.test.js
@@ -2,18 +2,14 @@ import isValidNumber from './index';
 
 describe('isValidNumber', () => {
   it('should return true if value is a valid number', () => {
-    const result = isValidNumber(0)
-      && isValidNumber(24)
-
-    expect(result).toBe(true);
+    expect(isValidNumber(0)).toBe(true);
+    expect(isValidNumber(24)).toBe(true);
   });
 
   it('should return false if value is not valid number', () => {
-    const result = isValidNumber(false)
-      || isValidNumber('')
-      || isValidNumber(null)
-      || isValidNumber(undefined);
-
-    expect(result).toBe(false);
+    expect(isValidNumber('')).toBe(false);
+    expect(isValidNumber('24')).toBe(false);
+    expect(isValidNumber(null)).toBe(false);
+    expect(isValidNumber(undefined)).toBe(false);
   });
 });
