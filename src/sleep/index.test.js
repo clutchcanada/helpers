24c852c2fd4654cfa753c32b1d8ff956227fe6b9
Ahmed Panju <ahmed.a.panju@gmail.com>
2020-02-04 13:36:56 -0500

    mili

--- src/sleep/index.test.js
+++ src/sleep/index.test.js
@@ -7,7 +7,7 @@ describe("sleep", () => {
 
     sleep(500).then(mockFunction);
 
-    //Should not call mockFunction if only 20 seconds have passed
+    //Should not call mockFunction if only 20 milliseconds have passed
     jest.advanceTimersByTime(20); 
     await Promise.resolve();
     expect(mockFunction).not.toHaveBeenCalled(); 
