0db33fbce1f9d339e38ddfce8e9e2a69fd237a1b
Craig Pullar <craigmichaelpullar@gmail.com>
2020-02-06 12:02:24 -0500

    switch arg order

--- src/async-filter/index.js
+++ src/async-filter/index.js
@@ -1,12 +1,12 @@
 import * as R from "ramda";
 
-const asyncFilter = R.curry(async (callback, array) => {
+const asyncFilter = R.curry(async (array, callback) => {
   const fail = Symbol('Denotes callback failure');
   const callbackResults = await Promise.all(array.map(async (item, index) => {
     const result = await callback(item, index, array);
     return result ? item : fail;
   }));
-  
+
   return callbackResults.filter(R.complement(R.equals(fail)));
 });
 
