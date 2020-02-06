02674fbd26ef065f138940630637ec989b072ebc
Craig Pullar <craigmichaelpullar@gmail.com>
2020-02-06 11:48:14 -0500

    dont try catch

--- src/async-filter/index.js
+++ src/async-filter/index.js
@@ -3,14 +3,10 @@ import * as R from "ramda";
 const asyncFilter = R.curry(async (callback, array) => {
   const fail = Symbol('Denotes callback failure');
   const callbackResults = await Promise.all(array.map(async (item, index) => {
-    try {
-      const result = await callback(item, index, array);
-      return result ? item : fail;
-    } catch (error) {
-      console.log(error);
-      return fail;
-    }
+    const result = await callback(item, index, array);
+    return result ? item : fail;
   }));
+  
   return callbackResults.filter(R.complement(R.equals(fail)));
 });
 
--- src/async-filter/index.test.js
+++ src/async-filter/index.test.js
@@ -13,17 +13,4 @@ describe("AsyncFilter", () => {
     const actual = await asyncFilter(doesValueMatchIndexAsync, testArray);
     expect(actual).toEqual(expectedResult);
   });
-
-  it('should filter out results that error', async () => {
-    const testError = (item) => new Promise((resolve) => {
-      if(item === 5) {
-        throw new Error("oops");
-      }
-      resolve(true);
-    });
-    const testArray = [0,1,2,5,4];
-    const expectedResult = [0,1,2,4];
-    const actualResult = await asyncFilter(testError, testArray);
-    expect(actualResult).toEqual(expectedResult);
-  });
 });
