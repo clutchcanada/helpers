c7e2dd30733a28d35dcb71ca19de1dce91cc8a82
Jesse Davda <jessedavda@gmail.com>
2020-04-14 15:40:13 -0400

    Formatting

--- src/hexToRgba/index.js
+++ src/hexToRgba/index.js
@@ -1,10 +1,9 @@
 const hexToRgba = ({ hex, alpha }) => {
-  if(typeof hex !== 'string' && typeof alpha !== 'number') {
+  if(typeof hex !== 'string' && typeof alpha !== 'number') 
     throw new TypeError('Supplied arguments are of wrong type');
-  }
 
   if(hex.length !== 7)
-      throw new Error('The hex string is the wrong length, please include the #');
+    throw new Error('The hex string is the wrong length, please include the #');
 
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
