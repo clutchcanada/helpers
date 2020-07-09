840d9214b940bd036ea5ed7619a9fc3754c51b73
Craig Pullar <craigmichaelpullar@gmail.com>
2020-07-09 14:28:00 -0400

    change logic a little

--- src/stripPhoneNumber/index.js
+++ src/stripPhoneNumber/index.js
@@ -1,9 +1,12 @@
 function stripPhoneNumber(phoneNumber) {
   if (typeof phoneNumber === 'string' || (phoneNumber instanceof String)) {
     const stripedNumber = phoneNumber.replace(/\D/g, '').trim();
-    if(stripedNumber.length >= 11) {
+    if(stripedNumber.length >= 11 || stripedNumber < 10) {
       return `+${stripedNumber}`;
     } else if (stripedNumber.length === 10) {
+      /*
+        If its 10 we assume its a North American number with no area code.
+      */
       return `+1${stripedNumber}`;
     }
   }
