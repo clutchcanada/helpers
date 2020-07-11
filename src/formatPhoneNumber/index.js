9b3b3b6fe4b8bfa8dca2579ae438912be1cec9eb
Craig Pullar <craigmichaelpullar@gmail.com>
2020-07-11 15:24:55 -0400

    update error

--- src/formatPhoneNumber/index.js
+++ src/formatPhoneNumber/index.js
@@ -6,7 +6,7 @@ export default (value) => {
     return value;
   }
   if(![String, Number].some(type => R.is(type, value) )) {
-    throwError("value passed to formatPhoneNumber must be a string");
+    throwError("Value passed to formatPhoneNumber must be a string or number");
   }
   const x = value.toString().replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
   return !x[2] ? x[1] : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ''}`;
