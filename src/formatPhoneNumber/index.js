f99baa5e4491a3b3491798cbbd07d33e24653ce4
Craig Pullar <craigmichaelpullar@gmail.com>
2020-07-11 15:23:27 -0400

    return null only if null

--- src/formatPhoneNumber/index.js
+++ src/formatPhoneNumber/index.js
@@ -2,8 +2,11 @@ import * as R from "ramda";
 import throwError from "../throwError";
 
 export default (value) => {
+  if(R.isNil(value)) {
+    return value;
+  }
   if(![String, Number].some(type => R.is(type, value) )) {
-    return null;
+    throwError("value passed to formatPhoneNumber must be a string");
   }
   const x = value.toString().replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
   return !x[2] ? x[1] : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ''}`;
