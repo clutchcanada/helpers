b12ea5e7d546f2531c508f450ed9e9e72cd2d0a4
Craig Pullar <craigmichaelpullar@gmail.com>
2020-07-22 11:03:07 -0400

    added removeNilProperties function

---
+++ src/cleanNulls/index.js
@@ -0,0 +1,18 @@
+function removeNilProperties(obj) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const propName in obj) {
+    if (
+      obj[propName] === null ||
+      obj[propName] === undefined ||
+      obj[propName] === ''
+    ) {
+      delete obj[propName];
+    } else if (typeof obj[propName] === 'object') {
+      // Recurse here if the property is another object.
+      removeNilProperties(obj[propName]);
+    }
+  }
+  return obj;
+}
+
+export default removeNilProperties;
