bfc3eefc8d3cd53e3b9b07c3a531858500366a2c
Craig Pullar <craigmichaelpullar@gmail.com>
2020-07-22 21:02:59 -0400

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    node: true,
+    jest: true,
+    jasmine: true,
+  },
+  globals: {},
+  rules: {
+    'lines-between-class-members': 0,
+    'no-else-return': 0,
+    'import/order': 0,
+    'implicit-arrow-linebreak': 0,
+    'no-multiple-empty-lines': 0,
+    'operator-linebreak': 0,
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'function-paren-newline': 0,
+    'prefer-destructuring': 0,
+    'object-curly-newline': 0
+  },
+};
