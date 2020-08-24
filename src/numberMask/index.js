6d099f70c3501b1fa9b5832f54575b2db16f996d
Craig Pullar <craigmichaelpullar@gmail.com>
2020-08-24 14:04:05 -0400

    add comment

--- src/numberMask/index.js
+++ src/numberMask/index.js
@@ -1,6 +1,8 @@
 import * as R from 'ramda';
 
+// Removes non numeric values but allows preceding - and decimal places
 const removeNonNumeric = value => value.replace(/(?<!^)-|[^0-9.-]+/g, '');
+
 const addCommasToNumber = (value = '') => value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
 
 const numberMask = R.pipe(
