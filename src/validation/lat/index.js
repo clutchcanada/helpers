60f117591cf66016a2848a24d8963642bf163f93
JossWritesCode <joscelynrose@gmail.com>
2021-03-17 13:58:02 -0400

    added comments with references to where I got the regex logic

--- src/validation/lat/index.js
+++ src/validation/lat/index.js
@@ -1,3 +1,5 @@
+// logic borrowed from https://github.com/validatorjs/validator.js/blob/master/src/lib/isLatLong.js
+
 const validateLat = (value) => {
   const latRegex = new RegExp(/^\(?[+-]?(90(\.0+)?|[1-8]?\d(\.\d+)?)$/);
   return latRegex.test(value);
--- src/validation/long/index.js
+++ src/validation/long/index.js
@@ -1,3 +1,5 @@
+// logic borrowed from https://github.com/validatorjs/validator.js/blob/master/src/lib/isLatLong.js
+
 const validateLong = (value) => {
   const longRegex = new RegExp(
     /^\s?[+-]?(180(\.0+)?|1[0-7]\d(\.\d+)?|\d{1,2}(\.\d+)?)\)?$/,
