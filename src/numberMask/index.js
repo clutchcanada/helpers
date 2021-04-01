5543ca8216b99b2a48cff8e5f82795c90b362eb1
Jordan Cain <jordan.cain@clutch.ca>
2021-04-01 08:51:23 -0400

    use older numberMask with toLocaleString for inserting commas

--- src/numberMask/index.js
+++ src/numberMask/index.js
@@ -1,15 +1,27 @@
+import * as R from 'ramda';
+
+const removeNonNumeric = (value) => value.replace(/[^0-9.]+/g, '');
+
 /**
- * Given a Number, return a masked version with commas and squash to 2 fractionals
+ * Given a Number, return a masked version with commas and round to 2 fractionals
  * @param {Number} value Number to be masked
  * @returns String
  */
 const numberMask = (value) => {
-  if (!value) return '';
-  if (value === '-') return '-';
-  const valueAsNumber = parseFloat(value);
-  const localeOptions = valueAsNumber % 1 !== 0 ? { minimumFractionDigits: 2 } : {};
+  if (['-', '', undefined, null].includes(value)) return value || '';
+  const stringValue = String(value);
+  const isNegative = stringValue.charAt(0) === '-';
 
-  return !Number.isNaN(valueAsNumber) ? valueAsNumber.toLocaleString('en-CA', localeOptions) : '';
+  /*
+    We did have a negative look behind regex but it is not
+    supported in safari so had to to do it more manually
+  */
+  const numberValue = isNegative ? `-${removeNonNumeric(R.tail(stringValue))}` : removeNonNumeric(stringValue);
+
+  const localeOptions = numberValue % 1 !== 0
+    ? { minimumFractionDigits: 2, maximumFractionDigits: 6 }
+    : {};
+  return Number(numberValue).toLocaleString('en-CA', localeOptions);
 };
 
 export default numberMask;
