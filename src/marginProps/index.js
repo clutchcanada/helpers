14e9e1895a2c418bf68c67333f0d0f4a2b1953c6
Joscelyn Stancek <joscelynrose@gmail.com>
2021-12-07 11:15:14 -0500

    feat: added a marginprops function

---
+++ src/marginProps/index.js
@@ -0,0 +1,8 @@
+const marginProps = ({
+  marginTop = 0,
+  marginRight = 0,
+  marginBottom = 0,
+  marginLeft = 0,
+}) => `margin: ${marginTop} ${marginRight} ${marginBottom} ${marginLeft};`;
+
+export default marginProps;
