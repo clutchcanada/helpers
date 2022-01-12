dd6124318ccdfb6822dda1097f9fd2084e719cb2
Joss <joscelynrose@gmail.com>
2022-01-12 11:39:58 -0500

    chore: added comments to explain the formateDateLong func

--- src/formatDateLong/index.js
+++ src/formatDateLong/index.js
@@ -1,4 +1,10 @@
-const formatDateLong = (date) =>
+/**
+ * Given a number representing the number of miliseconds since the Unix Epoch,
+ * returns a date formatted with day month and year
+ * @param {number} -14182940000
+ * @returns {string} July 20, 1969
+ */
+const formatDateLong = date =>
   new Date(date).toLocaleDateString('en', {
     month: 'long',
     day: 'numeric',
